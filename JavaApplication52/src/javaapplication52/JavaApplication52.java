24e542f1487a095dea7f4a62a22e89ace63df7eb
UPN <UPN@ULNLAB1420>
2019-12-07 10:38:03 -0500

    Envio de archivos en java

---
+++ JavaApplication52/src/javaapplication52/JavaApplication52.java
@@ -0,0 +1,12 @@
+
+package javaapplication52;
+
+public class JavaApplication52 {
+    
+    
+    public static void main(String[] args) {
+        System.out.println("hola world");
+        
+    }
+    
+}
